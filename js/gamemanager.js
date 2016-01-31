de086cd1139a1fdd571ceb801cf6c611d0f3b2fe
jonathanasdf <jonathanasdf@hotmail.com>
2016-01-30 22:31:10 -0500

    ninja fix

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -97,7 +97,7 @@ function GameManager() {
         if (nx != instance.playerX) instance.playerX = alignedx;
         if (ny != instance.playerY) instance.playerY = alignedy;
       }
-      this.updateEnemies(dt);
+      this.updateEnemies();
     } // if (instance.paused)
       
     // offset the view if the player is scrolling to the edge of the map
@@ -187,7 +187,7 @@ function GameManager() {
     PIXI.ticker.shared.add(gameLoop, instance);
   }
 
-  this.updateEnemies = function(dt) {
+  this.updateEnemies = function() {
     for(var e=0; e<instance.enemies.length; e++) {
       var ee = instance.enemies[e];
       if(ee.rotating === 0) {
